e10cdf47d8760e8044bacae5ad848ffb39c014f8
Muhammad <umar.dev23@gmail.com>
2025-02-20 10:15:54 +0500

    Com: 03: Implementd task 4

--- App.js
+++ App.js
@@ -1,23 +1,40 @@
 // App.js
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { useEffect } from "react";
+import { AppState } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import SignupScreen from './screens/SignupScreen';
-import LoginScreen from './screens/LoginScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import EditProfileScreen from './screens/EditProfileScreen';
+import SignupScreen from "./screens/SignupScreen";
+import LoginScreen from "./screens/LoginScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import EditProfileScreen from "./screens/EditProfileScreen";
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  useEffect(() => {
+    console.log("The app is Starting...");
+
+    // App State Listener
+    const handleAppStateChange = (nextAppState) => {
+      console.log("App state changed to:", nextAppState);
+    };
+
+    const subscription = AppState.addEventListener("change", handleAppStateChange);
+
+    return () => {
+      subscription.remove(); // Cleanup when component unmounts
+    };
+  }, []);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator 
+      <Stack.Navigator
         initialRouteName="Login"
         screenOptions={{
           headerShown: false,
-          cardStyle: { backgroundColor: '#fff' }
+          cardStyle: { backgroundColor: "#fff" },
         }}
       >
         <Stack.Screen name="Signup" component={SignupScreen} />
